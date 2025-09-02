e7303be3a994765c4a3319d74a4ad046bf775456
amirsnj <158460774+amirsnj@users.noreply.github.com>
2025-09-02 15:30:31 +0330

    add cors orgins variable

--- app/settings.py
+++ app/settings.py
@@ -74,10 +74,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
 ROOT_URLCONF = 'app.urls'
 
